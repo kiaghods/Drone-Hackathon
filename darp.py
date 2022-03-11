502f8d8a74afbe3277c619801beead94287a0e4f
Olivier_Idir <olivier.idir@ens-lyon.fr>
2022-03-11 15:01:33 +0100

    Revert "fusion sauvegardée, même"
    This reverts commit 8f3b7b3ee13fc07ff93015707113aaf060ebb22d.

--- darp.py
+++ darp.py
@@ -522,6 +522,7 @@ class DARP:
         return MMnew
 
     def IsThisAGoalState(self, thresh, connectedRobotRegions):
+        print("goal verification : weight repartition", self.ArrayOfElements, "and connectedness :", connectedRobotRegions)
         for r in range(self.droneNo):
             if np.absolute(self.DesireableAssign[r] - self.ArrayOfElements[r]) > thresh or not connectedRobotRegions[r]:
                 return False
